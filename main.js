ea63a3cba0ed66828377b8f12a0e1045abdc3594
Super3laa <superalaa28@gmail.com>
2022-01-25 23:25:41 +0200

    BasicFillAdded

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const colors = require('colors');
 const fs = require('fs');
 const path = require('path');
 const writeXlsxFile = require('write-excel-file/node')
-
 const days = 28;
+let Nbt4yatFollower = new Array(days).fill(0)
 const daysStart = ["Tue", "Wed", "Thu", "Fr ", "Sat", "Sun", "Mon"]
 let users = [
     {
@@ -68,7 +68,7 @@ let users = [
     }, {
         name: "Bahaa",
         dayOff: [5, 6, 7, 8, 9, 10, 11, 25, 26, 27, 28],//14-7
-        consultationDays: [1, 2, 3, 12, 13, 14, 15, 16, 17, 20, 23, 24, 4, 16],//Sat,Tue
+        consultationDays: [1, 2, 3, 17,18, 20,21, 23, 24, 4],//Sat,Tue
         Nbt4ya: 0,
         Nbt4yat: [],
         limit: 7,
@@ -76,7 +76,7 @@ let users = [
     }, {
         name: "Mosta",
         dayOff: [5, 6, 7, 8, 9, 19, 20, 21, 22, 23, 24, 25],//Hybrif
-        consultationDays: [1, 2, 3, 4, 10, 11, 12, 14, 13, 15, 17, 18, 26, 27],//Tue
+        consultationDays: [1, 2, 3, 4, 10, 11,12,13,14,15,16,  17,  26, 27,28],//Tue
         Nbt4ya: 0,
         Nbt4yat: [],
         limit: 7,
@@ -84,38 +84,16 @@ let users = [
     }
 ]
 let Table = [];
-for (let i = 0; i < days; i++) {
-    let currentDay = i + 1;
-    let PotentialUsersdToBeNbt4y = []
-    users.forEach((user, i) => {
-        let HighPriorityFlag = false
-        if (!user.dayOff.includes(currentDay + 1) && !user.dayOff.includes(currentDay)) { //Tomorrow is not day off and today not day off
-            user.consultationDays.forEach(potentialDay => {
-                if (potentialDay == currentDay && user.limit > user.Nbt4yat.length) {
-                    PotentialUsersdToBeNbt4y.push({ userIndex: i, priority: "High" });
-                    HighPriorityFlag = true
-                }
-            })
-            if (HighPriorityFlag == false && user.limit > user.Nbt4yat.length) PotentialUsersdToBeNbt4y.push({ userIndex: i, priority: "Low" });
-        }
-    })
 
-    try {
-        PotentialUsersdToBeNbt4y = customSort(PotentialUsersdToBeNbt4y)
-        users[PotentialUsersdToBeNbt4y[0].userIndex].Nbt4yat.push(i + 1)
-        users[PotentialUsersdToBeNbt4y[0].userIndex].Nbt4ya++;
-        users[PotentialUsersdToBeNbt4y[1].userIndex].Nbt4yat.push(i + 1)
-        users[PotentialUsersdToBeNbt4y[1].userIndex].Nbt4ya++;
-    } catch (error) {
-        console.log(currentDay);
-        console.log(PotentialUsersdToBeNbt4y)
-    }
-    //console.log(PotentialUsersdToBeNbt4y)
-}
-
+BasicFillNbt4yat()
+//Print();
+//console.log(Nbt4yatFollower)
+//checker for more than 2 will be nbt4ya
+FullFillNbt4yat()
+//console.log(users)
 Print();
+//console.log(Nbt4yatFollower)
 //FillTable();
-
 
 
 async function FillTable() {
@@ -124,23 +102,15 @@ async function FillTable() {
     })
 }
 
-
-
 function customSort(arr) {
     orders = ["High", "Low"]
     arr.sort((x, y) => users[x.userIndex].Nbt4ya - users[y.userIndex].Nbt4ya);
     return arr.sort((x, y) => orders.indexOf(x.priority) - orders.indexOf(y.priority));
 }
 
-
-
 function Print() {
     let row = [];
     process.stdout.write("Feb   ")
-    row.push({
-        value: 'February',
-        fontWeight: 'bold'
-    })
     for (let i = 0; i < days; i++) {
         process.stdout.write(" " + (i + 1) + (i + 1 > 9 ? "|" : " |"));
         row.push({
@@ -148,56 +118,75 @@ function Print() {
             fontWeight: 'bold'
         })
     }
-    Table.push(row); row = [];
     console.log("")
     process.stdout.write("Day   ")
-    row.push({
-        value: "Day",
-        fontWeight: "bold"
-    })
     for (let i = 0; i < days; i++) {
         process.stdout.write(daysStart[(i % 7 + 7) % 7] + "|");
-        row.push({
-            value: daysStart[(i % 7 + 7) % 7],
-            fontWeight: 'bold'
-        })
     }
-    Table.push(row); row = [];
     console.log("")
     users.forEach(user => {
         process.stdout.write(user.name + " ")
-        row.push({
-            value: user.name,
-            fontWeight: 'bold',
-        })
         for (let i = 0; i < days; i++) {
             if (user.dayOff.includes(i + 1)) {
                 process.stdout.write(" ر ".green.bold + "|")
-                row.push({
-                    value: 'ر',
-                    fontWeight: 'bold',
-                })
             } else if (user.Nbt4yat.includes(i + 1)) {
-                process.stdout.write(
-                    (user.consultationDays.includes(i + 1) ? ' ن '.red.bold : ' ن '.yellow.bold)
-                    + "|")
-                row.push({
-                    value: user.consultationDays.includes(i + 1) ? 'ن'.red.bold : 'ن'.yellow.bold,
-                    fontWeight: 'bold',
-                })
+                process.stdout.write((user.consultationDays.includes(i + 1) ? ' ن '.red.bold : ' ن '.yellow.bold) + "|")
             } else {
                 if (user.consultationDays.includes(i + 1) && user.type == "م") {
                     process.stdout.write(' س '.bold.blue + "|");
-                    row.push({ value: 'س',color:"#0000FF",align:"center" })
                 } else {
                     process.stdout.write(` ${user.type} |`)
-                    row.push({ value: `${user.type}` })
                 }
             }
         }
-        process.stdout.write(" " + user.Nbt4ya + " [" + user.Nbt4yat + "]")
+        process.stdout.write(" " + user.Nbt4ya /*+ " [" + user.Nbt4yat + "]"*/)
         Table.push(row); row = [];
-        console.log(""); 
+        console.log("");
 
     })
+}
+function BasicFillNbt4yat() {
+    users.forEach((user,i) => {
+        if (user.type == "م") {
+            users[i].Nbt4yat = [...user.consultationDays]
+            user.Nbt4yat.forEach(i => Nbt4yatFollower[i - 1]++);
+            users[i].Nbt4ya = user.consultationDays.length
+        }
+    })
+}
+function FullFillNbt4yat() {
+    for (let i = 0; i < days; i++) {
+        let currentDay = i + 1;
+        let PotentialUsersdToBeNbt4y = []
+        if (Nbt4yatFollower[currentDay - 1] != 2) {
+            users.forEach((user, i) => {
+                let HighPriorityFlag = false
+                if (!user.dayOff.includes(currentDay + 1) && !user.dayOff.includes(currentDay)) { //Tomorrow is not day off and today not day off
+                    user.consultationDays.forEach(potentialDay => {
+                        if (potentialDay == currentDay && user.limit > user.Nbt4yat.length) {
+                            PotentialUsersdToBeNbt4y.push({ userIndex: i, priority: "High" });
+                            HighPriorityFlag = true
+                        }
+                    })
+                    if (HighPriorityFlag == false && user.limit > user.Nbt4yat.length) PotentialUsersdToBeNbt4y.push({ userIndex: i, priority: "Low" });
+                }
+            })
+            try {
+                PotentialUsersdToBeNbt4y = customSort(PotentialUsersdToBeNbt4y)
+                users[PotentialUsersdToBeNbt4y[0].userIndex].Nbt4yat.push(i + 1)
+                users[PotentialUsersdToBeNbt4y[0].userIndex].Nbt4ya++;
+                Nbt4yatFollower[currentDay - 1]++;
+                if (Nbt4yatFollower[currentDay - 1] == 1) {
+                    users[PotentialUsersdToBeNbt4y[1].userIndex].Nbt4yat.push(i + 1)
+                    users[PotentialUsersdToBeNbt4y[1].userIndex].Nbt4ya++;
+                    Nbt4yatFollower[currentDay - 1]++;
+                }
+            } catch (error) {
+                console.log(currentDay);
+                console.log(PotentialUsersdToBeNbt4y)
+            }
+
+        }
+        //console.log(PotentialUsersdToBeNbt4y)
+    }
 }
